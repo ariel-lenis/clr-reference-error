f013936defb457724cfdbc626696b51a701033ed
Ariel Lenis <ariel.lenis@communitybrands.com>
2018-04-13 20:29:01 -0400

    Add project files.

---
+++ CLRLibraryA/CLRLibraryA.h
@@ -0,0 +1,15 @@
+// CLRLibraryA.h
+
+#pragma once
+
+using namespace System;
+using namespace InternalLibrary1;
+
+namespace CLRLibraryA {
+
+	public ref class Class1
+	{
+		Operations^ operations;
+		// TODO: Add your methods for this class here.
+	};
+}
